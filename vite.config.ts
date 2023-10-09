6c01cae95c5d267d3c5db7cbb2df23c3525e60dc
Jaime Xu <xu1545@purdue.edu>
2023-10-09 17:02:43 -0400

    changed base

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/jaimexu.dev/",
+  plugins: [react()],
+  base: "/",
   build: {
     outDir: "dist"
   },
-  plugins: [react()],
 })
